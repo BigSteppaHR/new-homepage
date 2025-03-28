6deaabcf1e200e4e40914d36b6a2bb0828c0e651
BigSteppaHR <henryc.reid66@gmail.com>
2025-03-27 20:14:41 -0400

    213123

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      minHeight: '100vh',
+      padding: '2rem',
+      textAlign: 'center',
+      fontFamily: 'system-ui, -apple-system, sans-serif'
+    }}>
+      <h1>Welcome to My Homepage</h1>
+      <p>This site is currently under construction</p>
+    </div>
+  )
+}
